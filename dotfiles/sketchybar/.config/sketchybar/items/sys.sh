7e1dd02396986fce639444ff7110d8e36d161dec
Octavio Montt <montt.octavio@gmail.com>
2025-04-30 12:10:31 +0100

    dejar la base

--- dotfiles/sketchybar/.config/sketchybar/items/sys.sh
+++ dotfiles/sketchybar/.config/sketchybar/items/sys.sh
@@ -2,38 +2,34 @@
 
 BACKGROUND_COLOR=$CYAN
 
+sets=(
+  padding_left=0
+  padding_right=0
+  label.padding_right=5
+  label.padding_left=0
+  icon.padding_right=3
+  icon.padding_left=3
+  icon.color=$BLACK
+  label.color=$BLACK
+  background.color=$BACKGROUND_COLOR
+)
+
 sketchybar  --add item volume right \
-            --set volume  icon.padding_right=0 \
-                          icon.padding_left=0 \
-                          label.padding_left=3 \
-                          icon.color=$BLACK \
-                          label.color=$BLACK \
-                          padding_left=0 \
-                          background.color=$BACKGROUND_COLOR \
+            --set volume  "${sets[@]}" \
                           script="$PLUGINS_DIR/volume.sh" \
-            --subscribe volume volume_change \
+            --subscribe volume volume_change
 
 sketchybar  --add item battery right \
             --set battery update_freq=180 \
                           script="$PLUGINS_DIR/battery.sh" \
-                          icon.padding_right=0 \
-                          icon.color=$BLACK \
-                          label.color=$BLACK \
-                          background.color=$BACKGROUND_COLOR \
-                          padding_left=0 \
-                          padding_right=0 \
-                          icon.padding_left=0 \
-                          label.padding_left=3 \
-            --subscribe battery system_woke power_source_change \
+                          "${sets[@]}" \
+            --subscribe battery system_woke power_source_change
 
-sketchybar --add item "sys.cpu" right \
-           --set "sys.cpu"  update_freq=2 \
-                      padding_right=0 \
-                      icon.padding_right=0 \
-                      icon.padding_right=0 \
-                      icon.color=$BLACK \
-                      label.color=$BLACK \
-                      background.color=$BACKGROUND_COLOR \
-                      label.padding_left=3 \
-                      icon=  \
-                      script="$PLUGINS_DIR/cpu.sh"
+sketchybar --add item cpu right \
+           --set cpu  update_freq=2 \
+                      script="$PLUGINS_DIR/cpu.sh" \
+                      "${sets[@]}"
+
+# NOTE: Tried to use brakets but didnt workd
+# sketchybar  --add bracket sys volume battery cpu \
+#             --set         sys background.color=$BACKGROUND_COLOR
